51d2e27b7de850fc2391ddf831672abe31a27915
darguelles.rojas91@gmail.com <darguelles.rojas91@gmail.com>
2019-01-14 23:18:13 -0200

    First commit

---
+++ src/main/java/com/updaytest/UpdArticlesApiApplication.java
@@ -0,0 +1,14 @@
+package com.updaytest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpdArticlesApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UpdArticlesApiApplication.class, args);
+    }
+
+}
+
