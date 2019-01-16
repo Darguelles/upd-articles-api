15ffd60675d41837474536b29d28b267dbdc8400
darguelles.rojas91@gmail.com <darguelles.rojas91@gmail.com>
2019-01-16 01:47:32 -0200

    Added constructors to author object to implement builder pattern

--- src/main/java/com/updaytest/model/Author.java
+++ src/main/java/com/updaytest/model/Author.java
@@ -1,8 +1,14 @@
 package com.updaytest.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Author {
 
     private String name;
